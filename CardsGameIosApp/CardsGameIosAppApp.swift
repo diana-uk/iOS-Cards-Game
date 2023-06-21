c0728c86b7a26cfae7b60308e5452f87f7020e1e
Diana Ukrainsky & Rivka Doskoch <rivstudies@gmail.con>
2023-06-21 21:24:22 +0300

    Initial Commit

---
+++ CardsGameIosApp/CardsGameIosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsGameIosAppApp.swift
+//  CardsGameIosApp
+//
+//  Created by Student22 on 21/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CardsGameIosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
